b0e1ccdf9f90647ef2c2c57a7bfea8ade7bc6cfb
hillmychen <zuixingshifen@gmail.com>
2019-11-01 06:49:36 +0800

    编译原理更新

--- design-pattern/design-pattern-demo/src/index.js
+++ design-pattern/design-pattern-demo/src/index.js
@@ -21,65 +21,109 @@
 // }
 // let haha=new Student("haha",20,60)
 // haha.write()
-class jQuery {
-    constructor(selector) {
-        let slice = Array.prototype.slice
-        let dom = slice.call(document.querySelectorAll(selector))
-        let len = dom ? dom.length : 0
-        for (let i = 0; i < len; i++) {
-            this[i] = dom[i]
-        }
-        this.length = len
-        this.selector = selector || ''
+
+/***************************************************************/
+// class jQuery {
+//     constructor(selector) {
+//         let slice = Array.prototype.slice
+//         let dom = slice.call(document.querySelectorAll(selector))
+//         let len = dom ? dom.length : 0
+//         for (let i = 0; i < len; i++) {
+//             this[i] = dom[i]
+//         }
+//         this.length = len
+//         this.selector = selector || ''
+//     }
+//     append(node) {
+
+//     }
+//     addClass(name) {
+
+//     }
+//     html(data) {
+
+//     }
+//     // 此处省略若干 API
+// }
+// window.$ = function (selector) {
+//     return new jQuery(selector)
+// }
+
+
+/***************************************************************/
+
+// //类 车
+// class Car{
+//     constructor(number,name){
+//         this.number=number
+//         this.name=name
+//     }
+// }
+// //快车
+// class quickCar extends Car{
+//     constructor(number,name){
+//         super(number,name)
+//         this.price=1
+//     }
+// }
+// //专车
+// class specialCar extends Car{
+//     constructor(number,name){
+//         super(number,name)
+//         this.price=10
+//     }
+// }
+// //行程
+// class Trip{
+//     constructor(car){
+//         this.car=car;
+//     }
+//     start(){
+//         console.log(`行程开始：${this.car.name} ,车牌号：${this.car.number}`)
+//     }
+//     end(){
+//         console.log(`行程结束：${this.car.name} ,车牌号：${this.car.number},使用了${this.car.price}元`)
+//     }
+// }
+// let newCar=new quickCar("123456","快车")
+// let trip=new Trip(newCar)
+// trip.start()
+// trip.end()
+/***************************************************************/
+
+
+class LoginForm {
+    constructor(){
+        this.state='hide'
     }
-    append(node) {
+    show(){
+        if(this.state==='show')
+            console.log('had shown');
+
+            return;
+        this.state='show'
+        console.log('showd');
 
     }
-    addClass(name) {
+    hide(){
+        if(this.state==='hide')
+            console.log('had hidden');
+            return;
+        this.state='hide'
+        console.log('hided');
 
     }
-    html(data) {
-
-    }
-    // 此处省略若干 API
 }
-window.$ = function (selector) {
-    return new jQuery(selector)
-}
-//类 车
-class Car{
-    constructor(number,name){
-        this.number=number
-        this.name=name
+LoginForm.getInstance=(()=>{
+    let instance;
+    return ()=>{
+        if(!instance){
+            instance=new LoginForm()
+        }   
+        return instance
     }
-}
-//快车
-class quickCar extends Car{
-    constructor(number,name){
-        super(number,name)
-        this.price=1
-    }
-}
-//专车
-class specialCar extends Car{
-    constructor(number,name){
-        super(number,name)
-        this.price=10
-    }
-}
-//行程
-class Trip{
-    constructor(car){
-        this.car=car;
-    }
-    start(){
-        console.log(`行程开始：${this.car.name} ,车牌号：${this.car.number}`)
-    }
-    end(){
-        console.log(`行程结束：${this.car.name} ,车牌号：${this.car.number},使用了${this.car.price}元`)
-    }
-}
-let newCar=new quickCar("123456","快车")
-let trip=new Trip(newCar)
-trip.start()
-trip.end()
+})()
+let LoginForm1=LoginForm.getInstance()
+LoginForm1.show()
+let LoginForm2=LoginForm.getInstance()
+LoginForm2.show()
